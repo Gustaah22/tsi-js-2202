05eb10abf19362ef96877f3b88bb8a1adb560675
Gustaah22 <nunesg404@gmail.com>
2022-09-16 22:10:22 -0300

    Dom continuação

--- DOM/ex2.js
+++ DOM/ex2.js
@@ -2,8 +2,33 @@ let btnCalcular = document.getElementById('calcular');
 
 btnCalcular.addEventListener ('click', function(evento){
     evento.preventDefault();
-    console.log(evento)
-})
+
+    let serv = document.getElementById('serviço');
+    let prazo = document.getElementById('prazo');
+    let res = document.getElementById('resultado');
+
+    //recuperar o dados da matriz
+    let preco = calculaPreco(serv.value, prazo.value);
+
+    //Mostra o resultado no elemento p
+    res.innerText = `O preço é ${preco}`;
+
+    //Ver dados do evento (click) no console
+    console.log(serv.value);
+
+    //ver dados do evenro (click) no console
+    console.log(evento);
+    
+
+    //Obter os valores de seviço e prazo
+});
+
+function calculaPreco(serviço, prazo){
+    let tabela = [['$400', '$250', '$100'],['$500', '$400', '$250' ],['Não Fazemos', 'Não Fazemos', '$400']];
+
+    return tabela[serviço][prazo];
+}
+
 
 /*
 btnCalcular.addEventListener ('click', executa(evento));
---
+++ DOM/ex3.js
@@ -0,0 +1,25 @@
+let e;
+
+//e = document.head;
+//e = document.body;
+//e = document.body.firstElementChild;
+//e = document.body.firstElementChild.firstElementChild;
+// e = document.doctype;
+// e = document.doctype.;
+//e = document.forms[0][1];
+// e = document.forms[0];
+// e = document.scripts;
+// e = document.scripts[2];
+
+
+// e = document.scripts[2].getAttribute('src');
+// e = document.getElementsByClassName('collection-item');
+// e = document.getElementById('task-title');
+//e = document.getElementById('main')
+// e = document.getElementsByClassName('card-action');
+
+// e = document.querySelector('div .row');
+e = document.querySelector('input#task');
+
+
+console.log(e);
