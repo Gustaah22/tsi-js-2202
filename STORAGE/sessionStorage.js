d04be6891656648f8f1c3decb62cf8b40a76b237
Gustaah22 <nunesg404@gmail.com>
2022-10-14 22:00:45 -0300

    Criando 'funçoes' para apagar e adicionar itens na lista

--- STORAGE/sessionStorage.js
+++ STORAGE/sessionStorage.js
@@ -21,3 +21,23 @@ document.querySelector('#mostrar').innerText = sessionStorage.getItem('dadoDoUsu
 //E para apagar no session storage?
 //removeItem()
 //sessionStorage.removeItem('dadoDoUsuario');
+
+let vetor = ['pera', 'banana', 'manga'];
+
+//Para guardar algo que não seja uma string no localStorage ou SessionStorage, temos que converter para string. Com o método JSON.stringify() transformamos vetores e objetos em string
+sessionStorage.setItem('frutas', JSON.stringify(vetor));
+
+//Para recuperar uma string JSON do localStorage ou sessionStorage e transformar em um objeto ou vetor novamente, use JSON.parse()
+let recuperaDado = JSON.parse(sessionStorage.getItem('frutas'));
+
+//Alterações no vetor recuperado do sessionStorage
+recuperaDado[3] = 'abacaxi';
+recuperaDado[1] = 'parchita'
+// console.log(recuperaDado);
+
+//Transforma o vetor em string para poder gravar em sessionStorage ou localStorage
+let strRecupDados = JSON.stringify(recuperaDado);
+console.log(strRecupDados);
+
+sessionStorage.setItem('frutas', strRecupDados);
+
