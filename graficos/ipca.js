b937d90ec2b501a581492c997ced226ff88cd2a5
Gustavo Nunes da Silva <gugaboy-@hotmail.com>
2022-11-18 23:15:55 -0300

    IPCA

--- graficos/ipca.js
+++ graficos/ipca.js
@@ -1,61 +1,73 @@
-document.querySelector('button').addEventListener('click', recuperaDados);
+const btn = document.querySelector('button').addEventListener('click', mostraIpca);
 
+let ano = parseInt(prompt('Escreva um ano:'));
 
-let jsonParaMorris;
+while(ano < 1992 || ano > 2022){
+    alert('Escreva um ano válido entre 1992 e 2022');
+    ano = parseInt(prompt('Escreva um ano:'));
+}
 
-const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101'
+let array = [1,2,4,'dada'];
 
-function recuperaDados(evento){
+let matriz = [[1,2,4,'dada'],[1,2,4,'dada'],[1,2,4,'dada'],[1,2,4,'dada']];
+matriz[1][3]
+
+function mostraIpca(evento){
     evento.preventDefault();
-
-    fetch(url).then(function(retorno){
-
-        return retorno.text();
-
-    }).then(function(stringJson){
-
-        const indices = JSON.parse(stringJson);
-
+    const url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201111';
+    
+    fetch(url)
+    .then(retorno => retorno.text())
+    .then(stringJson => {
+        let indices = JSON.parse(stringJson);
         let indice;
         let ipca = [];
+        indices.forEach(mes => {
+            let anoString = mes.data.substring(6,10);
+            let mesString = mes.data.substring(3,5);
+            let anoMesString = `${anoString}-${mesString}`;
+            /*let dataAtual = mes.data;
+            const [dia, mes, ano] = dataAtual.split('/');    
+            let dataNova = new Date(+ano, mes-1, +dia);
+            let dataFormatada = `${String(dataNova.getFullYear())}-${String((dataNova.getMonth() +1)).padStart(2, '0')}-${String((dataNova.getDate())).padStart(2, '0')}`
+            console.log(dataFormatada)
+            ipca.push({year: dataFormatada, value: mes.valor});*/
+            
+            if(parseInt(anoString) === ano){
+                indice = parseFloat(mes.valor);
 
-        indices.forEach(function(mes){
-            anoString = mes.data.substring(6, 10);
-            mesString = mes.data.substring(3, 5);
-            anoMesString = anoString + '-' + mesString;
-
-            /*
-            if(parseInt(mesString) != 2000{
+                ipca.push({month: anoMesString, value: indice});
                 return;
-            })
-            */
-
-
-            indice = parseFloat(mes.valor);
-
-            ipca.push({month: anoMesString, value: indice});
+            }
             
         });
+        let i = 1;
         
-        jsonParaMorris = {
-                            // ID do elemento onde o gráfico vai aparecer
-                            element: 'ipca',                         
-                            //dados do grafico 
-                            data: ipca,
-                                           
-                            // Nome do eixo com os  valores de X.
-                            xkey: 'month',
-                            // Uma lista de nome dos atributos de dados contidos no Y
-                            ykeys: ['value'],
-                            
-                            // Rótulos para os índices -- aparece quando passa o mouse por cima
-                            labels: ['IPCA'],
-                        }
-
-                        new Morris.Line(jsonParaMorris);
-
+        console.log(ipca)
+        const jsonMorris = {
+       
+            element: 'ipca',
         
-    }).catch(function(){
-        alert('API do banco central está fora do ar');
-    });
+            resize: true,    
+        
+            //Dados do gráfico
+            data: ipca,
+            
+            lineColors: ['CornflowerBlue'],
+            // Nome do eixo com os valores de X
+            xkey: 'month',
+
+            xLabels:'month',
+            
+            // Uma lista de nome dos atributos contidos no eixo Y
+            ykeys: ['value'],
+            //Rótulos para os indices que aparecem quando passa o mouse por cima
+            labels: ['IPCA']
+        }
+        // console.log(indices)
+        new Morris.Line(jsonMorris)
+    }).catch(() => {
+        alert('API do Banco Central fora do ar');
+    })
+    
 }
